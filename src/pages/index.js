26d147ee2dd5de3a7506a5d88fb775832fcd11b6
Jejo <jejomardorongon@gmail.com>
2020-06-19 11:04:01 +0800

    Developed card component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import React from "react"
-import { Container, FeatureImage } from "../components"
+import { Container, FeatureImage, Content, ContentCard } from "../components"
 
 const IndexPage = () => {
   return (
     <Container>
       <FeatureImage />
+      <Content>
+        <ContentCard
+          date="March 19, 1990"
+          title="Wheelie on the boy"
+          excerpt="lorem bus"
+          slug="/asdf"
+        />
+      </Content>
     </Container>
   )
 }
